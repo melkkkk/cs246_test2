23e358863638a4b986c6af274a54558e6a42876a
mcaringa <mcaringa@uwaterloo.ca>
2026-07-17 18:53:41 -0400

    going through command line args

--- sorcery.cc
+++ sorcery.cc
@@ -2,6 +2,31 @@ import <iostream>;
 
 using namespace std;
 
-int main(void){
+int main(int argc, char *argv []){
     cout << "Hello world!" << endl;
+
+    // for testing mode
+    bool testing = false;
+
+    // parsing through command line arguments
+    for (int i = 1; i < argc; ++i){
+        string command(argv[i]);
+        // cout << command << endl;
+        if (command == "-deck1"){
+            ++i;
+            // create deck for p1 from argv[i] file
+        } else if (command == "-deck2") {
+            ++i;
+            // create deck for p2 argv[i] file
+        } else if (command == "-init") {
+            ++i;
+            // read in commands from argv[i] file
+        } else if (command == "-testing") {
+            testing = true;
+        } else if (command == "-graphics") {
+            // activate graphics idk
+        } else {
+            cout << "'" << command << "' is an invalid command" << endl;
+        }
+    }
 }
